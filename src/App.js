e4fc52076929487bc47a0cd2bda39f8a50bfcd01
davidlealo <davidlealo@gmail.com>
2022-12-20 13:19:12 -0300

    Mejoras en el grid, limite de cantidad de caracteres de descripción

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
-import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 
 
@@ -39,29 +38,29 @@ export default function App() {
           <Routes>
             <Route path="/" element={<Home />} />
           </Routes>
-          <div>
-            {pizzas.map(
-              pizza => {
-                return (
-                  <Container fluid className='m-3'>
-                    <Row>
-                    <Card style={{ width: '18rem' }} key={pizza.id}>
-                      <Card.Img variant="top" src={pizza.img} className='m-1'/>
-                      <Card.Body>
-                        <Card.Title>{pizza.name}</Card.Title>
-                        <Card.Text>
-                          {pizza.desc}
-                        </Card.Text>
-                        <Button variant="warning" className='m-1'>Detalles</Button>
-                        <Button variant="danger" className='m-1'>Pedir</Button>
-                      </Card.Body>
-                    </Card>
-                    </Row>
-                  </Container>
-                )
-              }
-            )}
-          </div>
+          <Container fluid className='m-3 p-2' >
+            <Row className="justify-content-md-center">
+                {pizzas.map(
+                  pizza => {
+                    return (
+
+                      <Card style={{ width: '18rem' }} key={pizza.id} className='m-4'>
+                        <Card.Img variant="top" src={pizza.img} className='m-2' />
+                        <Card.Body>
+                          <Card.Title>{pizza.name}</Card.Title>
+                          <Card.Text>
+                            {pizza.desc.substring(0, 140)}
+                          </Card.Text>
+                          <Button variant="warning" className='m-1'>Detalles</Button>
+                          <Button variant="danger" className='m-1'>Pedir</Button>
+                        </Card.Body>
+                      </Card>
+
+                    )
+                  }
+                )}
+            </Row>
+          </Container>
         </BrowserRouter>
       </Context.Provider>
 
