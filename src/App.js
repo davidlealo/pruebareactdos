36590ce82d17795ccc0c46ef986d58c67dfa375e
davidlealo <davidlealo@gmail.com>
2022-12-20 05:13:39 -0300

    INstalación de CRA, de RRD y Bootstrap. Primera prueba con un H1 y un Button de Bootstrap

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Button from 'react-bootstrap/Button';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hola mundo!</h1>
+      <Button variant="warning">Warning</Button>
+    </div>
+  );
+}
+
+export default App;
